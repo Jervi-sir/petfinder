4669cf054a0fc9e2e8ff272936a04cfe78dbfdf8
unknown <Jervi>
2022-07-04 21:47:27 +0100

    edited selector menu

--- resources/views/components/_selector.blade.php
+++ resources/views/components/_selector.blade.php
@@ -1,17 +1,13 @@
 <div class="selector">
     <div class=""></div>
-    <div class="item">
-        <a href="{{ route('pet.all') }}">
-            All
-        </a>
-    </div>
+    <a href="{{ route('pet.all') }}" class="item">
+        <span>All </span>
+    </a>
 
     @foreach ($races as $race)
-    <div class="item {{ request()->is('race/' . $race->name) ? 'active' : '' }}" >
-        <a href="{{ route('pet.race', ['race' => $race->name]) }}">
-            <span>{{ $race->name }}</span>
-        </a>
-    </div>
+    <a href="{{ route('pet.race', ['race' => $race->name]) }}" class="item {{ request()->is('race/' . $race->name) ? 'active' : '' }}" >
+       <span>{{ $race->name }}</span>
+    </a>
     @endforeach
 
 </div>
