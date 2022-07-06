6763f381a94a1d9f87f51184b4a0ffa9b4519cdd
jervi <jervi>
2022-07-06 22:05:33 +0100

    created db table for translation

---
+++ database/seeders/TranslationSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class TranslationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('translations')->insert([
+            'english_word' => 'cat',
+            'translation' => 'cat chat قط chatton kitten kitty',
+        ]);
+        DB::table('translations')->insert([
+            'english_word' => 'dog',
+            'translation' => 'dog chien كلب pet doggy petty',
+        ]);
+        DB::table('translations')->insert([
+            'english_word' => 'black',
+            'translation' => 'black dark noir noire اسود اكحل كحل',
+        ]);
+        DB::table('translations')->insert([
+            'english_word' => 'black',
+            'translation' => 'black dark noir noire اسود اكحل كحل',
+        ]);
+        DB::table('translations')->insert([
+            'english_word' => 'siamoa',
+            'translation' => 'siamoa siamo سيامو سياموا',
+        ]);
+    }
+}
