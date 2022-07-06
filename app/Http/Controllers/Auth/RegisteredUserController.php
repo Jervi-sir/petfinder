e142b2b0646fed03aef40ecbf5e50e4e959b2458
unknown <Jervi>
2022-07-06 11:15:04 +0100

    used a intended for login redirection

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -53,8 +53,6 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        //return redirect(RouteServiceProvider::HOME);
-        return url()->previous();
-
+        return redirect();
     }
 }
