ea40b7b307adde4cc344c5adea06956ee068c3b7
jervi <gacembekhira@gmail.com>
2022-07-24 03:00:02 +0200

    https depends on env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultstringLength(191);
-        URL::forceScheme('https');
+        if(env('APP_ENV') != 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
